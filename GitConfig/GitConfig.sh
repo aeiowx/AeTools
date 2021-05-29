e17d88d6ebe41735d39dfd850b9690918c0566b0
zxw3221 <zxw3221@163.com>
2021-05-29 19:00:01 +0800

    update git config
    Signed-off-by: zxw3221 <zxw3221@163.com>

--- GitConfig/GitConfig.sh
+++ GitConfig/GitConfig.sh
@@ -2,6 +2,7 @@
 
  git config --global diff.tool vimdiff
  git config --global difftool.prompt false
+ git config --global difftool.trustExitCode true
  git config --global alias.d difftool
 
  git config --global core.editor "vim"
