d310c8ea9470317f41a9c034a2ace935e0b6cfe6
zhengxiaowei02 <zhengxiaowei02@baidu.com>
2021-11-29 18:47:24 +0800

    update vimconfig.sh

--- VimConfig/vimconfig.sh
+++ VimConfig/vimconfig.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ctags -I __THROW --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --fields=+S  -R -f ~/.vim/systags /usr/include /usr/local/include
+path=$1
+
+ctags -I __THROW --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --fields=+S  -R -f ~/.vim/systags /usr/include /usr/local/include $path
